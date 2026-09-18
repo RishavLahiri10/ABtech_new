4a1d6c443fc5313c985ca9eb33cf8ec269712a81
RishavLahiri10 <rishavlahiri132@gmail.com>
2026-09-19 00:18:21 +0530

    new fix

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({ esbuild: { jsx: 'automatic' } });
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    jsx: 'automatic',
+  },
+});
